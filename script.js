2c9572a0ae9fe16ba6679f6d27fb29a0e6fc9f4d
sizwe <nkosiczwe@gmail.com>
2022-04-07 18:16:19 +0200

    Add validate input date

---
+++ script.js
@@ -0,0 +1,9 @@
+const months = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+function ageCalculate(){
+    let today = new Date();
+    let inputDate = Date(document.getElementById("date-input").value);
+    let birthDay,birthMonth,birthYear;
+    
+
+}
