fb5d4d7a0f8de8bc76b3eb101ab900682a1a3608
sizwe <nkosiczwe@gmail.com>
2022-04-07 18:34:43 +0200

    Add validate input date part 2

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ function ageCalculate(){
     let today = new Date();
     let inputDate = Date(document.getElementById("date-input").value);
     let birthDay,birthMonth,birthYear;
-    
 
+    let birthDetails = {
+        date:inputDate.getDate(),
+        month:inputDate.getMonth() + 1,
+        year:inputDate.getFullYear()
+    }
+
+    let currentDate = today.getDate();
+    let currentMonth = today.getMonth();
+    let currentYear = today.getFullYear();
+
+    if(birthDetails.year > currentYear ||
+        (birthDetails.month > currentMonth &&
+            birthDetails.year == currentYear) ||
+            (birthDetails.date > currentDate &&
+                birthDetails.month == currentMonth && 
+                birthDetails.year == currentYear)
+                ){
+                    alert("Not Born Yet");
+                    return;
+                }
 }
